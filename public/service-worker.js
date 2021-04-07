b21647fefcf8663fa92d3dedbfa11e6f3c3f4190
ejsmith13 <ejsmithwork@gmail.com>
2021-04-06 20:18:27 -0400

    deleting unused code

--- public/service-worker.js
+++ public/service-worker.js
@@ -76,29 +76,3 @@ self.addEventListener("fetch", function (evt) {
   );
 });
 
-// --------------------------------------------------------------// install event handler
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open("static").then((cache) => {
-//       return cache.addAll([
-//         "./",
-//         "./index.html",
-//         "./style.css",
-//         "./icons/icon-192x192.png",
-//         "./icons/icon-512x512.png",
-//         "./db.js",
-//       ]);
-//     })
-//   );
-//   console.log("Install");
-//   self.skipWaiting();
-// });
-
-// // retrieve assets from cache
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
